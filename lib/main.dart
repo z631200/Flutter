daf9f1f6254d3a7ce6b5b31134fa0152c361b1f7
imsho <imsho0218@gmail.com>
2024-07-03 00:28:05 +0800

    add image

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,193 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      home: const NavigationExample(),
+      
+      );
+  }
+}
+
+class NavigationExample extends StatefulWidget {
+  const NavigationExample({super.key});
+
+  @override
+  State<NavigationExample> createState() => _NavigationExampleState();
+}
+
+class _NavigationExampleState extends State<NavigationExample> {
+  int currentPageIndex = 0;
+  NavigationDestinationLabelBehavior labelBehavior =
+      NavigationDestinationLabelBehavior.alwaysShow;
+
+  final List<Widget> pages = [
+    CourseManagementPage(),
+    PromptManagementPage(),
+    PersonalInfoPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: Color.fromARGB(255, 88, 228, 236),
+        labelBehavior: labelBehavior,
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.class_rounded),
+            icon: Icon(Icons.class_outlined),
+            label: '課程管理',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.design_services_rounded),
+            icon: Icon(Icons.design_services_outlined),
+            label: 'Prompt 管理',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.account_circle_rounded),
+            icon: Icon(Icons.account_circle_outlined),
+            label: '個人資訊',
+          ),
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: IndexedStack(
+        index: currentPageIndex,
+        children: pages,
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+// class CourseManagementPage extends StatelessWidget {
+//   const CourseManagementPage({super.key});
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+//   @override
+//   Widget build(BuildContext context) => Center(child: Text('課程管理 Page'));
+// }
 
-  final String title;
+class PromptManagementPage extends StatelessWidget {
+  const PromptManagementPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) => Center(child: Text('Prompt 管理 Page'));
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class PersonalInfoPage extends StatelessWidget {
+  const PersonalInfoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Image Example'),
+        ),
+        body: Center(
+          child: Image.asset('images/algo.png'),
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+}
+
+
+class CourseManagementPage extends StatelessWidget {
+  const CourseManagementPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(50),
+        child: GridView.count(
+          crossAxisCount: 4,
+          crossAxisSpacing: 75,
+          mainAxisSpacing: 75,
+          children: [
+            CourseTile(
+              title: '演算法',
+              // imageUrl: 'images/algo.png', // Replace with actual image URL
+              isDropdown: true,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            CourseTile(title: '演算法'),
+            CourseTile(title: '資料結構'),
+            CourseTile(title: '演算法'),
+            CourseTile(title: '資料結構'),
+            CourseTile(title: '演算法'),
+            AddCourseTile(),
+            
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class CourseTile extends StatelessWidget {
+  final String title;
+  final String? imageUrl;
+  final bool isDropdown;
+
+  CourseTile({required this.title, this.imageUrl, this.isDropdown = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (imageUrl != null)
+            Image.network(
+              imageUrl!,
+              height: 100,
+              width: 100,
+              fit: BoxFit.cover,
+            )
+          else
+            Icon(Icons.book, size: 50),
+          SizedBox(height: 8),
+          Text(title),
+          if (isDropdown) Icon(Icons.arrow_drop_down),
+        ],
+      ),
+    );
+  }
+}
+
+class AddCourseTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Handle add course action
+      },
+      child: Card(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.add, size: 50),
+            SizedBox(height: 8),
+            Text('新增課程'),
+          ],
+        ),
+      ),
     );
   }
 }
