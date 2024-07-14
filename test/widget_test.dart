217d46e5583021a775aa27557d83f9d0af3666b6
z631200 <z631200@gmail.com>
2024-07-14 22:23:30 +0800

    fix gitignore

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ncu_emi/main.dart';
-//test ignore
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
